a8961ac9c388b65d89d4562cd10be6dc2456eb83
vikram110703 <sainivks4321@gmail.com>
2023-06-16 22:58:49 +0530

    spelling mistake

--- controllers/user.js
+++ controllers/user.js
@@ -52,8 +52,8 @@ export const logout = (req, res) => {
     .status(200)
     .cookie("token", "", {
       expires: new Date(Date.now()),
-      sameSite: process.env.NODE_ENV === "Develpoment" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Develpoment" ? false : true,
+      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      secure: process.env.NODE_ENV === "Development" ? false : true,
     })
     .json({
       success: true,
